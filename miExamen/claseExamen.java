3a3fa5c0221a1fe1078c8ccd3ad7e53e90b4216b
luciagf23 <luciagf23@educastur.es>
2024-02-23 19:02:11 +0200

    Añadidos los primeros ficheros al proyecto

---
+++ miExamen/claseExamen.java
@@ -0,0 +1,18 @@
+package miExamen;
+
+/**
+ *
+ * @author alu11v
+ */
+public class claseExamen {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+        System.out.println("Estoy haciendo y haciendo el examen de la UT3");
+    }
+    
+}
