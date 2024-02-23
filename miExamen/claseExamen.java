454aa84bfde11fe177ccdf68be2e90a8131d010b
luciagf23 <luciagf23@educastur.es>
2024-02-23 19:16:43 +0200

    Añadidos comentarios de documentación en claseExamen

--- miExamen/claseExamen.java
+++ miExamen/claseExamen.java
@@ -11,7 +11,15 @@ public class claseExamen {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    
+    /**
+     *<h1>Entornos de desarrollo UT3</h1>
+     *<p>Esta clase sirve para realizar el examen de la UT3 del módulo Entornos de Desarrollo</p>    
+     *<href link=https://www.youtube.com/playlist?list=PLU8oAlHdN5BlyaPFiNQcV0xDqy0eR35aU> Curso de Git y github de píldoras informáticas</a>
+     *@author Lucia
+     *@version 1.0.0
+     *@since 23/02/2024
+     */   
+        
         System.out.println("Estoy haciendo y haciendo el examen de la UT3");
     }
     
